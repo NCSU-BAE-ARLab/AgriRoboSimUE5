8affd4a08aa137499f759dd6d24c2c21af432d2c
XingjianL <lixingjian2002@gmail.com>
2023-10-19 18:37:47 -0400

    UE 5.3 for learning agents plugin, also rename

---
+++ AgriRoboSim_5.3/Source/Test2/MyROSIntegrationGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ROSIntegrationGameInstance.h"
+#include "MyROSIntegrationGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST2_API UMyROSIntegrationGameInstance : public UROSIntegrationGameInstance
+{
+	GENERATED_BODY()
+	
+};
